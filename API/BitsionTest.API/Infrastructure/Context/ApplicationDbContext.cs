b94899cb0d33a7be8e915d68aec4ca354e35df0b
Wnorowsky, Nicolás <wnorowsky.nicolas@gmail.com>
2024-12-30 19:00:55 -0300

    Move to infastructure folder

---
+++ API/BitsionTest.API/Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BitsionTest.API.Models.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BitsionTest.API.Infrastructure.Context
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : IdentityDbContext<ApplicationUser>(options)
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<Client> Clients { get; set; }
+    }
+}
