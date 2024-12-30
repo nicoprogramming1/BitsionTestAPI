9ffd4a02b0ddc469f8a8bc0bb06eaf7cdd390364
Wnorowsky, Nicolás <wnorowsky.nicolas@gmail.com>
2024-12-30 19:02:05 -0300

    Add Token service

---
+++ API/BitsionTest.API/Services/Implementation/TokenServiceImpl.cs
@@ -0,0 +1,18 @@
+﻿using BitsionTest.API.Models.Domain.Entities;
+using BitsionTest.API.Services.Interface;
+
+namespace BitsionTest.API.Services.Implementation
+{
+    public class TokenServiceImpl : ITokenService
+    {
+        public string GenerateRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GenerateToken(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API/BitsionTest.API/Services/Interface/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using BitsionTest.API.Models.Domain.Entities;
+
+namespace BitsionTest.API.Services.Interface
+{
+    public interface ITokenService
+    {
+        Task<string> GenerateToken(ApplicationUser user);
+        string GenerateRefreshToken();
+    }
+}
