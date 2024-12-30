2b416e66c53051bd9085e273777d7cb01c2358c7
Wnorowsky, Nicolás <wnorowsky.nicolas@gmail.com>
2024-12-30 19:01:06 -0300

    Add initial mapping

---
+++ API/BitsionTest.API/Infrastructure/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BitsionTest.API.Models.Domain.Contracts;
+using BitsionTest.API.Models.Domain.Entities;
+
+namespace BitsionTest.API.Infrastructure.Mapping
+{
+    public class MappingProfile: Profile    // creamos reglas de mapeado a entidades
+    {
+        public MappingProfile() {
+            CreateMap<ApplicationUser, UserResponse>();
+            CreateMap<ApplicationUser, CurrentUserResponse>();
+            CreateMap<UserRegisterRequest, ApplicationUser>();
+        }
+    }
+}
