fdad4c48c9298162cce537944c706e83f17be0c8
Wnorowsky, Nicolás <wnorowsky.nicolas@gmail.com>
2024-12-30 19:03:08 -0300

    Move entities to Entities folder and add Identity to ApplicationUser

---
+++ API/BitsionTest.API/Models/Domain/Entities/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace BitsionTest.API.Models.Domain.Entities
+{
+    public class Role
+    {
+        public Guid Id { get; set; }
+        public required string RoleName { get; set; }
+    }
+}
