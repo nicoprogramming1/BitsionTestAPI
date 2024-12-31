1eb9f9dfc1219df114c03243b9b941f8a745533a
Wnorowsky, Nicolás <wnorowsky.nicolas@gmail.com>
2024-12-31 15:33:35 -0300

    Add RoleSeeder class to manage Roles in Identity (Admin, User)

---
+++ API/BitsionTest.API/Domain/Contracts/RoleSeeder.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BitsionTest.API.Domain.Contracts
+{
+    public static class RoleSeeder
+    {
+        // con esta clase vamos a gestionar los roles de los usuarios usando Identity
+        public static async Task SeedRolesAndAdminUser(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            // Creamos los dos roles
+            string[] roles = { "Admin", "User" };
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            // Crear un usuario administrador si no existe
+            var adminUser = await userManager.FindByEmailAsync("admin@bitsion.com");
+            if (adminUser == null)
+            {
+                var user = new IdentityUser
+                {
+                    Email = "admin@bitsion.com",
+                    EmailConfirmed = true
+                };
+                var result = await userManager.CreateAsync(user, "Admin123");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "Admin");
+                }
+            }
+        }
+    }
+}
