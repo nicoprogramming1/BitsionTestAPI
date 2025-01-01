b9e795f535a49804a5b6919536701f07f6d48e83
Wnorowsky, Nicolás <wnorowsky.nicolas@gmail.com>
2024-12-31 23:26:08 -0300

    Update Client class, not it has all the attributes from the Design Class Model (UML class structure)

---
+++ API/BitsionTest.API/Domain/Entities/Client.cs
@@ -0,0 +1,21 @@
+﻿namespace BitsionTest.API.Domain.Entities
+{
+    public class Client
+    {
+        public Guid Id { get; set; }
+        public string LongName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string? Nationality { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+        public bool CanDrive { get; set; }
+        public bool WearGlasses { get; set; }
+        public bool IsDiabetic { get; set; }
+        public string? OtherDiseases { get; set; }
+        public bool isDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
