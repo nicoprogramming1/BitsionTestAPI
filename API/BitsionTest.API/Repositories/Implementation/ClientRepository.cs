0823df8964d00b96581b2de3db10d4cb81e423a0
Wnorowsky, Nicolás <wnorowsky.nicolas@gmail.com>
2025-01-08 11:36:01 -0300

    Add soft delete instead a permament one to clients

--- API/BitsionTest.API/Repositories/Implementation/ClientRepository.cs
+++ API/BitsionTest.API/Repositories/Implementation/ClientRepository.cs
@@ -31,7 +31,33 @@ public class ClientRepository : IClientRepository
         return client;
     }
 
+
+    // soft delete, cambia el isDeleted de un client a true de forma tal que sea filtrado
+    // todas las querys quedando "marcado" como inactivo o borrado
     public async Task<bool> DeleteClientAsync(Guid id)
+    {
+        var client = await dbContext.Clients.FirstOrDefaultAsync(c => c.Id == id);
+        if (client == null)
+        {
+            throw new KeyNotFoundException("No se encontró el cliente con el ID especificado.");
+        }
+
+        // cambiamos el estado a true para lograr la eliminación lógica
+        client.isDeleted = true;
+
+        // mapeamos a cliente los datos a actualizar
+        dbContext.Entry(client).CurrentValues.SetValues(client);
+        await dbContext.SaveChangesAsync();
+        return true;
+    }
+
+
+    /**
+     * 
+     * Como vamos a implementar una eliminación lógica, no es necesario este método para un delete completo
+     * pero lo lo había hecho en primera instancia y lo dejo a modo de ofrecer una alternativa viable
+     * 
+     * public async Task<bool> DeleteClientAsync(Guid id)
     {
         var client = await dbContext.Clients.FirstOrDefaultAsync(c => c.Id == id);
         if (client == null)
@@ -43,6 +69,7 @@ public class ClientRepository : IClientRepository
         await dbContext.SaveChangesAsync();
         return true;
     }
+     */
 
 
 
